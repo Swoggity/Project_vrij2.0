6a5de183dbc8475f8df558d5376286e2ba3ed566
KristianHKU <127218454+KristianHKU@users.noreply.github.com>
2023-06-14 12:56:42 +0200

    Added the rebel and fatcat and some file structuring

---
+++ Assets/Prefabs/Enemies/Rebel/EnemyRebel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRebel : Enemy
+{
+    private float playerPosition;
+
+    public override void Die()
+    {
+        Quaternion rotation = Quaternion.Euler(-90f, 0f, 0f);
+        Instantiate(DeathEffect, transform.position, rotation);
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        if (co.isGamePaused()) return;
+        if (!isObstacleDetected)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        DetectObstacle();
+        playerPosition = playerObject.transform.position.x;
+    }
+
+    private void DetectObstacle()
+    {
+        if (transform.position.x <= playerPosition + adjustedDetectionDistance)
+        {
+            isObstacleDetected = true;
+        }
+        else if (transform.position.x >= playerPosition + adjustedDetectionDistance + 1)
+        {
+            isObstacleDetected = false;
+        }
+    }
+
+}
